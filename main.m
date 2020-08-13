0eefc5a9eee3086caa6558bf76ecf69079eceba5
David Meissner <davidpluseipi@protonmail.com>
2020-08-13 10:28:37 -0700

    cleaned up main.m and put everything on one plot

--- main.m
+++ main.m
@@ -1,5 +1,11 @@
 %% main
-
+%
+% variables
+% e = earth data
+% ma = mars data
+% me = mercury data
+% v = venus data
+% 
 %% start clean
 clear
 close all
@@ -8,91 +14,41 @@ home
 %% load the data
 load('orbits.mat'); % first 3 planets
 % contents of orbits.mat is a matrix called 'data'
-
+numPlanets = size(data,2)/2;
 % organize the data
+me = data(:,1:2); % mercury
+v = data(:,3:4); % venus
+e = data(:,5:6); % earth
 
-% mercury
-mx = data(:,1);
-my = data(:,2);
+%% plot the orbits and show where the planets are on a specific day
+dd = 127; % desired day
 
-m = data(:,1:2);
-
-% venus
-vx = data(:,3);
-vy = data(:,4);
-
-v = data(:,3:4);
-
-% earth
-ex = data(:,5);
-ey = data(:,6);
-
-e = data(:,5:6);
-
-%% plot the data (planets)
-
-% % mercury
-% figure(1)
-% plot(mx,my)
-% grid on
-% title('mercury');
-% 
-% % venus
-% figure(2)
-% plot(vx,vy)
-% grid on
-% title('venus');
-% 
-% % earth
-% figure(3)
-% plot(ex,ey)
-% grid on
-% title('earth')
-
-% loop the inner planets
-m_period = sum(~isnan(m(:,1)));
-iterations = floor(length(e)/m_period);
-extra = rem(length(e),m_period);
-append_this = m(1:m_period, :);
-m2 = zeros(size(e));
-for i = 1:iterations
-    m2 = [ m(1:i*m_period, :); append_this];
-end
-m2 = [m; m(1:extra,:)];
-clear m
-m = m2;
-
-%% show where the planets are or will be
-
-% dd = input('Enter day to show (integer): '); % desired day
-dd = 127
-
-%% single plot
-figure(12)
+figure(1)
 hold on
 grid on
-d = zeros(9,1);
-for i = 1:3
-    period = sum(~isnan(data(:,i)));
-    d(i) = dd;
-    while d(i) > period
-        d(i) = d(i) - period;
-    end
-    plot(data(:,2*i-1), data(:, 2*i))
-    plot(data(d(i),2*i-1), data(d(i), 2*i), 'm*')
-end
-% % venus
-% plot(vx,vy)
-% plot(v(dd,1), v(dd,2), 'm*')
-% 
-% % earth
-% plot(ex,ey)
-% plot(e(dd,1), e(dd,2), 'm*')
 
+d = zeros(numPlanets,1); % create a vector to store "right sized" dd's
+for i = 1:numPlanets
+    
+    period = sum(~isnan(data(:,i))); % sum up 1s for each non-nan
+    d(i) = dd; % store desired day in the vector d for that planet
+    
+    while d(i) > period       % while the desired day is > period
+        d(i) = d(i) - period; % decrease the desired day by the period
+    end                       % now we can get actual values from data
+    
+    plot(data(:,2*i-1), data(:, 2*i)) % plot the orbit 
+                                      % 2i-1 means odd columns
+                                      % 2i   means even columns
+    plot(data(d(i),2*i-1), data(d(i), 2*i), 'm*') % plot the planet
+    
+end
+
+% place a textbox with the desired day on the upper left
 annotation('textbox', [0.15 0.85 0.07 0.05],...
     'String',num2str(dd), 'FitBoxToText','off');
 
-%% get parameters for each ellipse (major and minor axes, and eccentricity)
+%% get parameters for each ellipse (major/minor axes & eccentricity)
 
 for i = [1 3 5]
     period = sum(~isnan(data(:,i)));
@@ -100,18 +56,18 @@ for i = [1 3 5]
     [row_of_minor_axis, row_of_major_axis] = helper(period, data(:,i:i+1));
     
     figure(i)
-    %minor
-    B(1,1) = m(row_of_minor_axis,1);
-    B(1,2) = m(row_of_minor_axis,2);
-    B(2,1) = m(row_of_minor_axis + period/2,1);
-    B(2,2) = m(row_of_minor_axis + period/2,2);
+    % minor
+    B(1,1) = me(row_of_minor_axis,1);
+    B(1,2) = me(row_of_minor_axis,2);
+    B(2,1) = me(row_of_minor_axis + period/2,1);
+    B(2,2) = me(row_of_minor_axis + period/2,2);
     plot(B(:,1), B(:,2))
     
-    %major
-    A(1,1) = m(row_of_major_axis,1);
-    A(1,2) = m(row_of_major_axis,2);
-    A(2,1) = m(row_of_minor_axis + period/2,1);
-    A(2,2) = m(row_of_minor_axis + period/2,2);
+    % major
+    A(1,1) = me(row_of_major_axis,1);
+    A(1,2) = me(row_of_major_axis,2);
+    A(2,1) = me(row_of_major_axis + period/2,1);
+    A(2,2) = me(row_of_major_axis + period/2,2);
     plot(A(:,1), A(:,2))
     
 end
